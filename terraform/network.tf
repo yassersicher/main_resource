07d989dac68e03bc3a3ed084d590dac7cb862904
yasser <yab@hylastix.com>
2025-07-23 08:11:59 +0200

    vnet_new  commit

---
+++ terraform/network.tf
@@ -0,0 +1,25 @@
+
+
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "example-network"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+ 
+
+  subnet {
+    name             = "subnet1"
+    address_prefixes = ["10.0.1.0/24"]
+  }
+
+  subnet {
+    name             = "subnet2"
+    address_prefixes = ["10.0.2.0/24"]
+   
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
